ed803ad2dc7824bfa621b51abbd239ac37c9516a
mininik08inv <mininik08inv@yandex.ru>
2025-03-05 06:48:34 +0300

    небольшие изменения в моделях

---
+++ football_team/team/models.py
@@ -0,0 +1,113 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название команды")
+    description = models.TextField(verbose_name="Описание команды")
+    matches_played = models.IntegerField(default=0, verbose_name="Сыграно матчей")
+    matches_won = models.IntegerField(default=0, verbose_name="Победы")
+    matches_drawn = models.IntegerField(default=0, verbose_name="Ничьи")
+    matches_lost = models.IntegerField(default=0, verbose_name="Поражения")
+    photos = models.ManyToManyField('TeamPhoto', blank=True, related_name='teams')
+    videos = models.ManyToManyField('TeamVideo', blank=True, related_name='teams')
+
+    class Meta:
+        verbose_name = "Команда"
+        verbose_name_plural = "Команды"
+
+    def __str__(self):
+        return self.name
+
+
+class TeamPhoto(models.Model):
+    image = models.ImageField(upload_to='team_photos/', verbose_name="Фото команды")
+    description = models.TextField(blank=True, null=True, verbose_name="Описание фото")
+
+    class Meta:
+        verbose_name = "Командные фото"
+        verbose_name_plural = "Командные фото"
+
+    def __str__(self):
+        return self.image.name
+
+class TeamVideo(models.Model):
+    video = models.FileField(upload_to='videos/', verbose_name="Командные видео")
+    description = models.TextField(blank=True, null=True, verbose_name="Описание видео")
+
+    class Meta:
+        verbose_name = "Командные видео"
+        verbose_name_plural = "Командные видео"
+
+    def __str__(self):
+        return self.video.name
+
+
+class Player(models.Model):
+    POSITION_CHOICES = [
+        ('GK', 'Вратарь'),
+        ('DF', 'Защитник'),
+        ('MF', 'Полузащитник'),
+        ('FW', 'Нападающий'),
+    ]
+
+    last_name = models.CharField(max_length=100, verbose_name="Фамилия")
+    first_name = models.CharField(max_length=100, verbose_name="Имя")
+    patronymic = models.CharField(max_length=100, blank=True, verbose_name="Отчество")
+    date_of_birth = models.DateField(verbose_name="Дата рождения")
+    main_photo = models.ImageField(upload_to='player_photos/', blank=True, verbose_name="Основное фото")
+    team = models.ForeignKey('Team', on_delete=models.CASCADE, related_name='players', verbose_name="Команда")
+    position = models.CharField(max_length=2, choices=POSITION_CHOICES, verbose_name="Амплуа")
+    last_modified = models.DateTimeField(auto_now=True, verbose_name="Дата последнего изменения")
+
+    class Meta:
+        verbose_name = "Игрок"
+        verbose_name_plural = "Игроки"
+
+    def __str__(self):
+        return f'{self.last_name} {self.first_name}'
+
+    def get_absolute_url(self):
+        return reverse('player_detail', kwargs={'player_id': self.pk})
+
+
+class Photo(models.Model):
+    image = models.ImageField(upload_to='photos/', verbose_name="Фото")
+    description = models.TextField(blank=True, null=True, verbose_name="Описание фото")
+    player = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='additional_photos', verbose_name="Игрок")
+
+    class Meta:
+        verbose_name = "Фото игроков"
+        verbose_name_plural = "Фото игроков"
+
+    def __str__(self):
+        return self.image.name
+
+
+class Video(models.Model):
+    video = models.FileField(upload_to='videos/', verbose_name="Видео")
+    description = models.TextField(blank=True, null=True, verbose_name="Описание видео")
+    player = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='additional_videos', verbose_name="Игрок")
+
+    class Meta:
+        verbose_name = "Видео игроков"
+        verbose_name_plural = "Видео игроков"
+
+    def __str__(self):
+        return self.video.name
+
+class Coach(models.Model):
+    first_name = models.CharField(max_length=100, verbose_name="Имя")
+    last_name = models.CharField(max_length=100, verbose_name="Фамилия")
+    patronymic = models.CharField(max_length=100, blank=True, verbose_name="Отчество")
+    photo = models.ImageField(upload_to='coach_photos/', blank=True, verbose_name="Фото тренера")
+    date_of_birth = models.DateField(blank=True, null=True, verbose_name="Дата рождения")
+    start_of_coaching_career = models.DateField(blank=True, null=True, verbose_name="Дата начала тренерской деятельности")
+    team = models.ForeignKey('Team', on_delete=models.CASCADE, verbose_name="Команда")
+
+    class Meta:
+        verbose_name = "Тренер"
+        verbose_name_plural = "Тренеры"
+
+    def __str__(self):
+        return f'{self.last_name} {self.first_name} {self.patronymic}'
---
+++ football_team/team/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Team, Player, Coach
+
+
+def team_detail(request):
+    team = Team.objects.first()  # Получаем первую команду
+    if not team:
+        return render(request, 'team/team_detail.html',
+                      {'team': None, 'players_by_position': {}, 'coaches': [], 'team_photos': [], 'team_videos': []})
+
+    # Группируем игроков по амплуа
+    players_by_position = {
+        'Вратари': team.players.filter(position='GK'),
+        'Защитники': team.players.filter(position='DF'),
+        'Полузащитники': team.players.filter(position='MF'),
+        'Нападающие': team.players.filter(position='FW'),
+    }
+    coaches = team.coach_set.all()
+
+    # Получаем все фото и видео команды
+    team_photos = team.photos.all()  # Список всех фото команды
+    team_videos = team.videos.all()  # Список всех видео команды
+
+    print(team_photos, team_videos)
+
+    return render(request, 'team/team_detail.html', {
+        'team': team,
+        'players_by_position': players_by_position,
+        'coaches': coaches,
+        'team_photos': team_photos,
+        'team_videos': team_videos,
+    })
+
+
+def player_detail(request, player_id):
+    player = get_object_or_404(Player, id=player_id)
+    main_photo = player.main_photo  # Основное фото игрока
+    additional_photos = player.additional_photos.all()  # Дополнительные фото
+    additional_videos = player.additional_videos.all()  # Видео игрока
+
+    return render(request, 'team/player_detail.html', {
+        'player': player,
+        'main_photo': main_photo,
+        'additional_photos': additional_photos,
+        'additional_videos': additional_videos,
+    })
